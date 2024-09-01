d6334ff2dc73322df47152ea88a5f6f44daee7ef
Mattia Sacchi <mattiasacchi2002@gmail.com>
2024-09-01 16:47:45 +0200

    added workspace

--- src/main/java/com/resource/CapsulaResource.java
+++ src/main/java/com/resource/CapsulaResource.java
@@ -1,10 +1,9 @@
 package com.resource;
+
 import java.util.Optional;
-import java.util.Random;
 
 import org.eclipse.californium.core.CoapResource;
 import org.eclipse.californium.core.coap.MediaTypeRegistry;
-import org.eclipse.californium.core.coap.Response;
 import org.eclipse.californium.core.coap.CoAP;
 import org.eclipse.californium.core.coap.CoAP.ResponseCode;
 import org.eclipse.californium.core.server.resources.CoapExchange;
@@ -15,37 +14,34 @@ import com.utils.CoreInterfaces;
 import com.utils.SenMLPack;
 import com.utils.SenMLRecord;
 
-public class CapsulaResource extends CoapResource{
+public class CapsulaResource extends CoapResource {
     private Gson gson;
     private static final String OBJECT_TITLE = "CapsulaSensor";
     Caspula temp;
     private static final Number SENSOR_VERSION = 0.1;
     private String deviceId;
 
-    public CapsulaResource( String name, String deviceId)
-    {
+    public CapsulaResource(String name, String deviceId) {
         super(name);
         getAttributes().setTitle(OBJECT_TITLE);
         gson = new Gson();
-        temp  = new Caspula();
+        temp = new Caspula();
         this.deviceId = deviceId;
 
-        // Init 
-        getAttributes().addAttribute("rt","com.resource.CaspulaResource");
-        getAttributes().addAttribute("if",CoreInterfaces.CORE_S.getValue());
-        getAttributes().addAttribute("ct",Integer.toString((MediaTypeRegistry.APPLICATION_SENML_JSON)));
-        getAttributes().addAttribute("ct",Integer.toString(MediaTypeRegistry.TEXT_PLAIN));
-
+        // Init
+        getAttributes().addAttribute("rt", "com.resource.CaspulaResource");
+        getAttributes().addAttribute("if", CoreInterfaces.CORE_S.getValue());
+        getAttributes().addAttribute("ct", Integer.toString((MediaTypeRegistry.APPLICATION_SENML_JSON)));
+        getAttributes().addAttribute("ct", Integer.toString(MediaTypeRegistry.TEXT_PLAIN));
 
     }
 
-    private Optional<String> getJsonSenMlResponse()
-    {
+    private Optional<String> getJsonSenMlResponse() {
         try {
 
             SenMLPack pack = new SenMLPack();
             SenMLRecord record = new SenMLRecord();
-            record.setBn(deviceId); 
+            record.setBn(deviceId);
             record.setN(getName());
             record.setVb(temp.getValue());
             record.setT(temp.getTimestamp());
@@ -59,32 +55,31 @@ public class CapsulaResource extends CoapResource{
     }
 
     @Override
-    public void handleGET(CoapExchange exchange)
-    {
+    public void handleGET(CoapExchange exchange) {
         try {
             temp.isThere();
-            
-            if(!(exchange.getRequestOptions().getAccept() == MediaTypeRegistry.APPLICATION_SENML_JSON
-            || exchange.getRequestOptions().getAccept() == MediaTypeRegistry.APPLICATION_JSON))
-            {
-                exchange.respond(CoAP.ResponseCode.CONTENT, String.valueOf(temp.getValue()),MediaTypeRegistry.TEXT_PLAIN);
+
+            if (!(exchange.getRequestOptions().getAccept() == MediaTypeRegistry.APPLICATION_SENML_JSON
+                    || exchange.getRequestOptions().getAccept() == MediaTypeRegistry.APPLICATION_JSON)) {
+                exchange.respond(CoAP.ResponseCode.CONTENT, String.valueOf(temp.getValue()),
+                        MediaTypeRegistry.TEXT_PLAIN);
                 return;
             }
 
             Optional<String> senMlPayload = getJsonSenMlResponse();
-            if(senMlPayload.isPresent())
-                exchange.respond(CoAP.ResponseCode.CONTENT, senMlPayload.get(),MediaTypeRegistry.APPLICATION_SENML_JSON);
+            if (senMlPayload.isPresent())
+                exchange.respond(CoAP.ResponseCode.CONTENT, senMlPayload.get(),
+                        MediaTypeRegistry.APPLICATION_SENML_JSON);
             else
                 exchange.respond(ResponseCode.INTERNAL_SERVER_ERROR);
-            
+
         } catch (Exception e) {
             exchange.respond(ResponseCode.INTERNAL_SERVER_ERROR);
         }
     }
 
     @Override
-    public void handlePOST(CoapExchange exchange)
-    {
+    public void handlePOST(CoapExchange exchange) {
         System.out.println("Chiamato");
         try {
             temp.setCapsul(true);
@@ -94,8 +89,5 @@ public class CapsulaResource extends CoapResource{
             exchange.respond(ResponseCode.INTERNAL_SERVER_ERROR);
         }
     }
-    
-    
+
 }
-
-
--- src/main/java/com/resource/TemperatureResource.java
+++ src/main/java/com/resource/TemperatureResource.java
@@ -1,10 +1,10 @@
 package com.resource;
+
 import java.util.Optional;
 
 import org.eclipse.californium.core.CoapResource;
 import org.eclipse.californium.core.coap.CoAP;
 import org.eclipse.californium.core.coap.MediaTypeRegistry;
-import org.eclipse.californium.core.coap.Response;
 import org.eclipse.californium.core.coap.CoAP.ResponseCode;
 import org.eclipse.californium.core.server.resources.CoapExchange;
 
@@ -14,10 +14,7 @@ import com.utils.CoreInterfaces;
 import com.utils.SenMLPack;
 import com.utils.SenMLRecord;
 
-import javafx.scene.media.Media;
-import com.utils.SenMLPack;
-
-public class TemperatureResource extends CoapResource{
+public class TemperatureResource extends CoapResource {
     private Gson gson;
     private static final String OBJECT_TITLE = "TemperatureSensor";
     private Temperature temp;
@@ -25,29 +22,27 @@ public class TemperatureResource extends CoapResource{
     private String UNIT = "Cel";
     private String deviceId;
 
-    public TemperatureResource( String name, String deviceId)
-    {
+    public TemperatureResource(String name, String deviceId) {
         super(name);
         getAttributes().setTitle(OBJECT_TITLE);
         gson = new Gson();
-        temp  = new Temperature();
+        temp = new Temperature();
         this.deviceId = deviceId;
-        
+
         // Init
 
-        getAttributes().addAttribute("rt","com.resource.TemperatureResource");
-        getAttributes().addAttribute("if",CoreInterfaces.CORE_S.getValue());
-        getAttributes().addAttribute("ct",Integer.toString((MediaTypeRegistry.APPLICATION_SENML_JSON)));
-        getAttributes().addAttribute("ct",Integer.toString(MediaTypeRegistry.TEXT_PLAIN));
+        getAttributes().addAttribute("rt", "com.resource.TemperatureResource");
+        getAttributes().addAttribute("if", CoreInterfaces.CORE_S.getValue());
+        getAttributes().addAttribute("ct", Integer.toString((MediaTypeRegistry.APPLICATION_SENML_JSON)));
+        getAttributes().addAttribute("ct", Integer.toString(MediaTypeRegistry.TEXT_PLAIN));
     }
 
-    private Optional<String> getJsonSenMlResponse()
-    {
+    private Optional<String> getJsonSenMlResponse() {
         try {
 
             SenMLPack pack = new SenMLPack();
             SenMLRecord record = new SenMLRecord();
-            record.setBn(deviceId); 
+            record.setBn(deviceId);
             record.setN(getName());
             record.setU(UNIT);
             record.setV(this.temp.getValue());
@@ -62,27 +57,27 @@ public class TemperatureResource extends CoapResource{
     }
 
     @Override
-    public void handleGET(CoapExchange exchange)
-    {
+    public void handleGET(CoapExchange exchange) {
         try {
             temp.measure();
-            
-            if(!(exchange.getRequestOptions().getAccept() == MediaTypeRegistry.APPLICATION_SENML_JSON
-            || exchange.getRequestOptions().getAccept() == MediaTypeRegistry.APPLICATION_JSON))
-            {
-                exchange.respond(CoAP.ResponseCode.CONTENT, String.valueOf(temp.getValue()),MediaTypeRegistry.TEXT_PLAIN);
+
+            if (!(exchange.getRequestOptions().getAccept() == MediaTypeRegistry.APPLICATION_SENML_JSON
+                    || exchange.getRequestOptions().getAccept() == MediaTypeRegistry.APPLICATION_JSON)) {
+                exchange.respond(CoAP.ResponseCode.CONTENT, String.valueOf(temp.getValue()),
+                        MediaTypeRegistry.TEXT_PLAIN);
                 return;
             }
 
             Optional<String> senMlPayload = getJsonSenMlResponse();
-            if(senMlPayload.isPresent())
-                exchange.respond(CoAP.ResponseCode.CONTENT, senMlPayload.get(),MediaTypeRegistry.APPLICATION_SENML_JSON);
+            if (senMlPayload.isPresent())
+                exchange.respond(CoAP.ResponseCode.CONTENT, senMlPayload.get(),
+                        MediaTypeRegistry.APPLICATION_SENML_JSON);
             else
                 exchange.respond(ResponseCode.INTERNAL_SERVER_ERROR);
-            
+
         } catch (Exception e) {
             exchange.respond(ResponseCode.INTERNAL_SERVER_ERROR);
         }
     }
-    
+
 }
